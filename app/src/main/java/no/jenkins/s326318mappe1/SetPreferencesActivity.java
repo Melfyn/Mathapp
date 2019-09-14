0376d7d4f488e4b723ed5d8a400550d5684d3fdf
Melfyn <melfyn1990@hotmail.com>
2019-09-14 19:47:37 +0200

    Language settins implemented

---
+++ app/src/main/java/no/jenkins/s326318mappe1/SetPreferencesActivity.java
@@ -0,0 +1,65 @@
+package no.jenkins.s326318mappe1;
+
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class SetPreferencesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,new PrefsFragment()).commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.registerOnSharedPreferenceChangeListener(myPreferenceListener);
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    public void changeLanguage(String language){
+        Resources res=getResources();
+        DisplayMetrics dm=res.getDisplayMetrics();
+        Configuration cf=res.getConfiguration();
+        cf.setLocale(new Locale(language));
+        res.updateConfiguration(cf,dm);
+    }
+
+    SharedPreferences.OnSharedPreferenceChangeListener myPreferenceListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+            String value = sharedPreferences.getString(s, "");
+            switch (value) {
+                case "no": {
+                    SetPreferencesActivity.this.changeLanguage("no");
+                    SetPreferencesActivity.this.recreate();
+                }
+
+                case "de": {
+                    SetPreferencesActivity.this.changeLanguage("de");
+                    SetPreferencesActivity.this.recreate();
+                }
+            }
+        }
+    };
+
+}
